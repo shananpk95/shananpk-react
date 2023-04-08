18b5cdf988e027a4547296694c032037496425f3
shananpk95 <shananpk95@gmail.com>
2023-04-08 12:15:50 +0530

    first

---
+++ App.js
@@ -0,0 +1,12 @@
+const content = React.createElement("div", {id: "first"},
+[
+    React.createElement("div", {id: "red"},
+        [React.createElement("h1", {style: {color: "red"}},"I am H1 red"),
+        React.createElement("h2", {style: {color: "red"}},"I am H2 red")]
+        ),
+    React.createElement("div", {id: "green"},
+        [React.createElement("h1", {style: {color: "green"}},"I am H1 green"),React.createElement("h2", {style: {color: "green"}},"I am H2 green")])
+]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(content);
